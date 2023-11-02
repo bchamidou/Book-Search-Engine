9aa3ec019cbe2ee5e59663387c388282d27bc5d9
Cheik H Bagagnan <bchamidou@gmail.com>
2023-11-02 02:17:38 -0400

    update book search engine starter code

---
+++ server/schemas/index.js
@@ -0,0 +1,4 @@
+const typeDefs = require('./typeDefs');
+const resolvers = require('./revolvers');
+
+module.exports = { typeDefs, resolvers };
---
+++ server/server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+
+// import ApolloServer
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const { authMiddleware } = require('./utils/auth');
+const path = require('path'); 
+
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+  const startApolloServer = async () => {
+    await server.start();
+  
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+  
+    app.use('/graphql', expressMiddleware(server));
+  
+    // if we're in production, serve client/dist as static assets
+    if (process.env.NODE_ENV === 'production') {
+      app.use(express.static(path.join(__dirname, '../client/dist')));
+  
+      app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      });
+    }
+  
+    db.once('open', () => {
+      app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+        console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+      });
+    });
+
+};
+
+startApolloServer();
